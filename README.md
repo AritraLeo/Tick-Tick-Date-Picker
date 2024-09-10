59c5a198e08e19609f422dacb0b4b45ed0f041e2
AritraLeo <aritrabinance056@gmail.com>
2024-09-10 12:57:07 +0530

    Test running (some failing lib issue) and added video link

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Tick-Tick-Date-Picker
+
 A custom reusable date picker component using Next.js, Tailwind CSS, and React Context API for state management
+
+# Video Link to the project: [G-Drive Link](https://drive.google.com/file/d/1B00-yhgeVxDAoUnOJoghTnugZLgCF1Ww/view?usp=sharing)
--- date-picker-component/jest.config.js
+++ date-picker-component/jest.config.js
@@ -1,10 +1,28 @@
+// module.exports = {
+//     preset: 'ts-jest',
+//     testEnvironment: 'jsdom',
+//     transform: {
+//         '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript and TSX files
+//         '^.+\\.(js|jsx)$': 'babel-jest', // Transform JavaScript and JSX files
+//     },
+//     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+//     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Your Jest setup file
+// };
+
+
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript and TSX files
-        '^.+\\.(js|jsx)$': 'babel-jest', // Transform JavaScript and JSX files
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Your Jest setup file
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.json',  // Make sure Jest uses the correct TypeScript configuration
+            isolatedModules: true, // Speeds up tests without type-checking
+        },
+    },
 };
